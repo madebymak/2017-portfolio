319fad058d1d68fa92b167dd01e0e39c2b6c3d7d
madebymak <mr.jmak@gmail.com>
2017-01-21 19:03:29 -0800

    Add responsive  background-img.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Jacky from './IMG_3137.JPG';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="App brd">
+        <div className="brd">
+          {/* <img src={Jacky}></img> */}
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
