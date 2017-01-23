52282fc3c8be8976d6af4ed30382136cb791a499
madebymak <mr.jmak@gmail.com>
2017-01-22 18:31:40 -0800

    Add email button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import About from './About.js';
 import Projects from './Projects.js';
-
+import Header from './Header.js';
 
 class App extends Component {
   render() {
@@ -13,6 +13,7 @@ class App extends Component {
       <Grid className="App brd">
         <Row className="brd">
           <Col className="brd content" xs={6}>
+            <Header/>
             <About/>
           </Col>
           <Col className="brd content" xs={6}>
