1ed96b31433bbce0376f267d29e2917a51f9e7d5
madebymak <mr.jmak@gmail.com>
2017-01-26 16:39:46 -0800

    Add responsive img in gallery component and styling layout.

--- src/Gallery.js
+++ src/Gallery.js
@@ -6,6 +6,7 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Modal from 'react-bootstrap/lib/Modal';
+import Image from 'react-bootstrap/lib/Image';
 
 class Gallery extends Component {
 
@@ -53,43 +54,46 @@ class Gallery extends Component {
         <Row className="brd">
 
           <Col className="brd gallery" xs={12} md={4} onClick={this.showModal1}>
-            <img src="http://placehold.it/250x150" alt="alt"></img>
-            <Modal id="project1" dialogClassName="modal-dialog" show={this.state.showModal1}>
+            <Image src="http://placehold.it/500x300" alt="alt" responsive/>
+            <Modal dialogClassName="modal-dialog" show={this.state.showModal1}>
               <Modal.Header>
                 {close}
               </Modal.Header>
-              <Modal.Body>
-                <img src="http://placehold.it/250x150" alt="alt"></img>
+              <Modal.Body className="brd">
                 <h2>Project #1</h2>
+                <Image className="full-img" src="http://placehold.it/500x300" alt="alt" responsive/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <h3>www.linkhere.com</h3>
               </Modal.Body>
             </Modal>
           </Col>
 
           <Col className="brd gallery" xs={12} md={4} onClick={this.showModal2}>
-            <img src="http://placehold.it/250x150" alt="alt"></img>
-            <Modal id="project1" dialogClassName="modal-dialog" show={this.state.showModal2}>
+            <Image src="http://placehold.it/500x300" alt="alt" responsive/>
+            <Modal dialogClassName="modal-dialog" show={this.state.showModal2}>
               <Modal.Header>
                 {close}
               </Modal.Header>
               <Modal.Body>
-                <img src="http://placehold.it/250x150" alt="alt"></img>
                 <h2>Project #2</h2>
+                <Image className="full-img" src="http://placehold.it/500x300" alt="alt" responsive/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <h3>www.linkhere.com</h3>
               </Modal.Body>
             </Modal>
           </Col>
 
           <Col className="brd gallery" xs={12} md={4} onClick={this.showModal3}>
-            <img src="http://placehold.it/250x150" alt="alt"></img>
-            <Modal id="project1" dialogClassName="modal-dialog" show={this.state.showModal3}>
+            <Image src="http://placehold.it/500x300" alt="alt" responsive/>
+            <Modal dialogClassName="modal-dialog" show={this.state.showModal3}>
               <Modal.Header>
                 {close}
               </Modal.Header>
               <Modal.Body>
-                <img src="http://placehold.it/250x150" alt="alt"></img>
                 <h2>Project #3</h2>
+                <Image className="full-img" src="http://placehold.it/500x300" alt="alt" responsive/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <h3>www.linkhere.com</h3>
               </Modal.Body>
             </Modal>
           </Col>
