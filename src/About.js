947fa5d9b16cc7d8b52cb9b39e85beeed22ffc96
madebymak <mr.jmak@gmail.com>
2017-01-28 15:47:11 -0800

    testing update

--- src/About.js
+++ src/About.js
@@ -23,7 +23,7 @@ class About extends Component {
       <div className="brd bio overlay" id="about">
         <Button className="closebtn" onClick={this.close}><Glyphicon glyph="remove"/></Button>
         <div className="brd overlay-content">
-          <h1>About Me</h1>
+          <h1>About Me test</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
