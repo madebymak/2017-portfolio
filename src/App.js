aa05c4ce021c12ea9930b3b1b20aa19a74b0f3d0
madebymak <mr.jmak@gmail.com>
2017-02-07 18:23:09 -0800

    Added projects images.

--- src/App.js
+++ src/App.js
@@ -8,10 +8,25 @@ import Projects from './Projects.js';
 // import Header from './Header.js';
 
 class App extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      hover: false,
+    };
+    this.toggleHover = this.toggleHover.bind(this);
+  }
+
+  toggleHover() {
+    // this.setState({ hover: true});
+    // console.log("entered");
+  };
+
   render() {
+
     return (
       <Grid className="App brd">
-        <Row className="brd">
+        <Row className="brd" onMouseOver={this.toggleHover} onMouseOut={this.state.hover}>
           <Col className="brd content" xs={6}>
             {/* <Header/> */}
             <About/>
--- src/Gallery.js
+++ src/Gallery.js
@@ -8,6 +8,10 @@ import Col from 'react-bootstrap/lib/Col';
 import Modal from 'react-bootstrap/lib/Modal';
 import Image from 'react-bootstrap/lib/Image';
 
+import Wiki from './img/wiki_where.png';
+import Tweetr from './img/tweetr.png';
+import Chatty from './img/chatty.png';
+
 class Gallery extends Component {
 
   constructor(props, context) {
@@ -54,44 +58,55 @@ class Gallery extends Component {
         <Row className="brd">
 
           <Col className="brd gallery" xs={12} md={4} onClick={this.showModal1}>
-            <Image src="http://placehold.it/500x300" alt="alt" responsive/>
+            <div className="thumb">
+              <Image src={Wiki} alt="alt" responsive/>
+            </div>
+
             <Modal dialogClassName="modal-dialog" backdrop="static" show={this.state.showModal1}>
               <Modal.Header>
                 {close}
               </Modal.Header>
               <Modal.Body className="brd">
                 <h2>Project #1</h2>
-                <Image className="full-img" src="http://placehold.it/500x300" alt="alt" responsive/>
+                <div className="full-img">
+                <Image src={Wiki} alt="alt" responsive/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <h3>www.linkhere.com</h3>
+              </div>
               </Modal.Body>
             </Modal>
           </Col>
 
           <Col className="brd gallery" xs={12} md={4} onClick={this.showModal2}>
-            <Image src="http://placehold.it/500x300" alt="alt" responsive/>
+            <div className="thumb">
+              <Image src={Tweetr} alt="alt" responsive/>
+            </div>
             <Modal dialogClassName="modal-dialog" show={this.state.showModal2}>
               <Modal.Header>
                 {close}
               </Modal.Header>
               <Modal.Body>
                 <h2>Project #2</h2>
-                <Image className="full-img" src="http://placehold.it/500x300" alt="alt" responsive/>
+                <div className="">
+                <Image src={Tweetr} alt="alt" responsive/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <h3>www.linkhere.com</h3>
+              </div>
               </Modal.Body>
             </Modal>
           </Col>
 
           <Col className="brd gallery" xs={12} md={4} onClick={this.showModal3}>
-            <Image src="http://placehold.it/500x300" alt="alt" responsive/>
+            <div className="thumb">
+              <Image src={Chatty} alt="alt" responsive/>
+            </div>
             <Modal dialogClassName="modal-dialog" show={this.state.showModal3}>
               <Modal.Header>
                 {close}
               </Modal.Header>
               <Modal.Body>
                 <h2>Project #3</h2>
-                <Image className="full-img" src="http://placehold.it/500x300" alt="alt" responsive/>
+                <Image src={Chatty} alt="alt" responsive/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <h3>www.linkhere.com</h3>
               </Modal.Body>
