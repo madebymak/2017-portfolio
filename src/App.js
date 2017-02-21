3d41b246d7f8e9bd95efbcbf0d3e40480dc4c755
madebymak <mr.jmak@gmail.com>
2017-02-20 16:08:23 -0800

    Removed code not being used anymore.

--- src/App.js
+++ src/App.js
@@ -5,30 +5,15 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import About from './About.js';
 import Projects from './Projects.js';
-// import Header from './Header.js';
 
 class App extends Component {
-
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      hover: false,
-    };
-    this.toggleHover = this.toggleHover.bind(this);
-  }
-
-  toggleHover() {
-    // this.setState({ hover: true});
-    // console.log("entered");
-  };
 
   render() {
 
     return (
       <Grid className="App brd">
-        <Row className="brd" onMouseOver={this.toggleHover} onMouseOut={this.state.hover}>
+        <Row className="brd">
           <Col className="brd content" xs={6}>
-            {/* <Header/> */}
             <About/>
           </Col>
           <Col className="brd content" xs={6}>
